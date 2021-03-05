149e09caa39ace352de8e936bb15e0a77d179310
haplo <haplo@haplolabs.io>
2021-03-05 12:09:19 -0700

    Updated fonts in config.h

--- config.h
+++ config.h
@@ -11,7 +11,10 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:pixelsize=14:antialias=true:autohint=true" };
+static const char *fonts[]          = {
+	"JetBrains Mono:pixelsize=16:antialias=true:autohint=true",
+	"EmojiOne:pixelsize=16:antialias=true:autohint=true",
+};
 static const char dmenufont[]       = "JetBrains Mono:pixelsize=14:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
