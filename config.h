31331fa8e730c8913a4c29690dddfeb258eec974
haplo <haplo@haplolabs.io>
2021-03-31 07:15:44 -0600

    Removed fullgaps patch

--- config.h
+++ config.h
@@ -2,7 +2,6 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 15;        /* gaps between windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -143,9 +142,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period,           focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,            tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,           tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,            setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,            setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,            setgaps,        {.i = 0  } },
 	{ MODKEY|ShiftMask,						  XK_Return,           togglescratch,  {.ui = 0 } },
 	{ MODKEY,            			      XK_r,	               togglescratch,  {.ui = 1 } },
 	{ MODKEY,            			      XK_apostrophe,	     togglescratch,  {.ui = 2 } },
